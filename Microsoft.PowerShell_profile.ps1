9d489378d12fd34a0a84cd505df5ea395e61e877
邓磊 <denglei1024@foxmail.com>
2026-04-03 14:08:45 +0800

    备份

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,360 +5,35 @@ using namespace System.Management.Automation.Language
 $OutputEncoding = [System.Text.Encoding]::UTF8
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
+# 引入模块
 Import-Module Terminal-Icons -ErrorAction SilentlyContinue
 Import-Module posh-git -ErrorAction SilentlyContinue
 Import-Module PSReadLine -ErrorAction SilentlyContinue
-Install-Module -Name CompletionPredictor
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\material.omp.json" | Invoke-Expression
+Import-Module z -ErrorAction SilentlyContinue
+Import-Module oh-my-posh -ErrorAction SilentlyContinue
 
-
-# 打开当前目录
-function Open-CurrentDirectory {
-    param (
-        [string]$path = (Get-Location)
-    )
-    Start-Process "explorer.exe" $path
-}
-
-Set-Alias open Open-CurrentDirectory
-
-Set-PSReadLineOption -PredictionViewStyle ListView
+#region 配置 PSReadLine 选项和键绑定
 Set-PSReadLineOption -EditMode Windows
+# 设置命令预测显示样式为列表视图
+Set-PSReadLineOption -PredictionViewStyle ListView
+# 设置历史搜索时光标移动到行尾
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -ShowToolTips
+# 设置命令预测来源为历史记录
+Set-PSReadLineOption -PredictionSource History
+# 设置 UpArrow 和 DownArrow 键的功能为历史搜索
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-Set-PSReadLineKeyHandler -Chord 'Ctrl+f' -Function ForwardWord
+# 设置 Ctrl+z 键的功能为撤销
+Set-PSReadLineKeyHandler -Chord 'Ctrl+z' -Function Undo
+# 设置 Enter 键的功能为验证并接受当前行的输入
 Set-PSReadLineKeyHandler -Chord 'Enter' -Function ValidateAndAcceptLine
-Set-PSReadLineKeyHandler -Key Tab -Function Complete
+# 设置 Ctrl+d 键的功能为退出 PowerShell
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function ViExit
+# 设置 Tab 键的补全功能为菜单补全
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+#endregion
 
-Set-PSReadLineKeyHandler -Key Ctrl+C -Function Copy
-Set-PSReadLineKeyHandler -Key Ctrl+V `
-                         -BriefDescription PasteAsHereString `
-                         -LongDescription "Paste the clipboard text as a here string" `
-                         -ScriptBlock {
-    param($key, $arg)
-
-    Add-Type -Assembly PresentationCore
-    if ([System.Windows.Clipboard]::ContainsText())
-    {
-        # Get clipboard text - remove trailing spaces, convert \r\n to \n, and remove the final \n.
-        $text = ([System.Windows.Clipboard]::GetText() -replace "\p{Zs}*`r?`n","`n").TrimEnd()
-        [Microsoft.PowerShell.PSConsoleReadLine]::Insert("@'`n$text`n'@")
-    }
-    else
-    {
-        [Microsoft.PowerShell.PSConsoleReadLine]::Ding()
-    }
-}
-
-Set-PSReadLineKeyHandler -Chord 'Ctrl+d,Ctrl+c' -Function CaptureScreen
-
-Set-PSReadLineKeyHandler -Key Alt+d -Function ShellKillWord
-Set-PSReadLineKeyHandler -Key Alt+Backspace -Function ShellBackwardKillWord
-Set-PSReadLineKeyHandler -Key Alt+b -Function ShellBackwardWord
-Set-PSReadLineKeyHandler -Key Alt+f -Function ShellForwardWord
-Set-PSReadLineKeyHandler -Key Alt+B -Function SelectShellBackwardWord
-Set-PSReadLineKeyHandler -Key Alt+F -Function SelectShellForwardWord
-
-Set-PSReadLineKeyHandler -Key F7 `
-                         -BriefDescription History `
-                         -LongDescription 'Show command history' `
-                         -ScriptBlock {
-    $pattern = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$pattern, [ref]$null)
-    if ($pattern)
-    {
-        $pattern = [regex]::Escape($pattern)
-    }
-
-    $history = [System.Collections.ArrayList]@(
-        $last = ''
-        $lines = ''
-        foreach ($line in [System.IO.File]::ReadLines((Get-PSReadLineOption).HistorySavePath))
-        {
-            if ($line.EndsWith('`'))
-            {
-                $line = $line.Substring(0, $line.Length - 1)
-                $lines = if ($lines)
-                {
-                    "$lines`n$line"
-                }
-                else
-                {
-                    $line
-                }
-                continue
-            }
-
-            if ($lines)
-            {
-                $line = "$lines`n$line"
-                $lines = ''
-            }
-
-            if (($line -cne $last) -and (!$pattern -or ($line -match $pattern)))
-            {
-                $last = $line
-                $line
-            }
-        }
-    )
-    $history.Reverse()
-
-    $command = $history | Out-GridView -Title History -PassThru
-    if ($command)
-    {
-        [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-        [Microsoft.PowerShell.PSConsoleReadLine]::Insert(($command -join "`n"))
-    }
-}
-
-#region Smart Insert/Delete
-
-# The next four key handlers are designed to make entering matched quotes
-# parens, and braces a nicer experience.  I'd like to include functions
-# in the module that do this, but this implementation still isn't as smart
-# as ReSharper, so I'm just providing it as a sample.
-
-Set-PSReadLineKeyHandler -Key '"',"'" `
-                         -BriefDescription SmartInsertQuote `
-                         -LongDescription "Insert paired quotes if not already on a quote" `
-                         -ScriptBlock {
-    param($key, $arg)
-
-    $quote = $key.KeyChar
-
-    $selectionStart = $null
-    $selectionLength = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$selectionStart, [ref]$selectionLength)
-
-    $line = $null
-    $cursor = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-
-    # If text is selected, just quote it without any smarts
-    if ($selectionStart -ne -1)
-    {
-        [Microsoft.PowerShell.PSConsoleReadLine]::Replace($selectionStart, $selectionLength, $quote + $line.SubString($selectionStart, $selectionLength) + $quote)
-        [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selectionStart + $selectionLength + 2)
-        return
-    }
-
-    $ast = $null
-    $tokens = $null
-    $parseErrors = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$parseErrors, [ref]$null)
-
-    function FindToken
-    {
-        param($tokens, $cursor)
-
-        foreach ($token in $tokens)
-        {
-            if ($cursor -lt $token.Extent.StartOffset) { continue }
-            if ($cursor -lt $token.Extent.EndOffset) {
-                $result = $token
-                $token = $token -as [StringExpandableToken]
-                if ($token) {
-                    $nested = FindToken $token.NestedTokens $cursor
-                    if ($nested) { $result = $nested }
-                }
-
-                return $result
-            }
-        }
-        return $null
-    }
-
-    $token = FindToken $tokens $cursor
-
-    # If we're on or inside a **quoted** string token (so not generic), we need to be smarter
-    if ($token -is [StringToken] -and $token.Kind -ne [TokenKind]::Generic) {
-        # If we're at the start of the string, assume we're inserting a new string
-        if ($token.Extent.StartOffset -eq $cursor) {
-            [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$quote$quote ")
-            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-            return
-        }
-
-        # If we're at the end of the string, move over the closing quote if present.
-        if ($token.Extent.EndOffset -eq ($cursor + 1) -and $line[$cursor] -eq $quote) {
-            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-            return
-        }
-    }
-
-    if ($null -eq $token -or
-        $token.Kind -eq [TokenKind]::RParen -or $token.Kind -eq [TokenKind]::RCurly -or $token.Kind -eq [TokenKind]::RBracket) {
-        if ($line[0..$cursor].Where{$_ -eq $quote}.Count % 2 -eq 1) {
-            # Odd number of quotes before the cursor, insert a single quote
-            [Microsoft.PowerShell.PSConsoleReadLine]::Insert($quote)
-        }
-        else {
-            # Insert matching quotes, move cursor to be in between the quotes
-            [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$quote$quote")
-            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-        }
-        return
-    }
-
-    # If cursor is at the start of a token, enclose it in quotes.
-    if ($token.Extent.StartOffset -eq $cursor) {
-        if ($token.Kind -eq [TokenKind]::Generic -or $token.Kind -eq [TokenKind]::Identifier -or 
-            $token.Kind -eq [TokenKind]::Variable -or $token.TokenFlags.hasFlag([TokenFlags]::Keyword)) {
-            $end = $token.Extent.EndOffset
-            $len = $end - $cursor
-            [Microsoft.PowerShell.PSConsoleReadLine]::Replace($cursor, $len, $quote + $line.SubString($cursor, $len) + $quote)
-            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($end + 2)
-            return
-        }
-    }
-
-    # We failed to be smart, so just insert a single quote
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert($quote)
-}
-
-Set-PSReadLineKeyHandler -Key '(','{','[' `
-                         -BriefDescription InsertPairedBraces `
-                         -LongDescription "Insert matching braces" `
-                         -ScriptBlock {
-    param($key, $arg)
-
-    $closeChar = switch ($key.KeyChar)
-    {
-        <#case#> '(' { [char]')'; break }
-        <#case#> '{' { [char]'}'; break }
-        <#case#> '[' { [char]']'; break }
-    }
-
-    $selectionStart = $null
-    $selectionLength = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$selectionStart, [ref]$selectionLength)
-
-    $line = $null
-    $cursor = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-    
-    if ($selectionStart -ne -1)
-    {
-      # Text is selected, wrap it in brackets
-      [Microsoft.PowerShell.PSConsoleReadLine]::Replace($selectionStart, $selectionLength, $key.KeyChar + $line.SubString($selectionStart, $selectionLength) + $closeChar)
-      [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selectionStart + $selectionLength + 2)
-    } else {
-      # No text is selected, insert a pair
-      [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)$closeChar")
-      [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-    }
-}
-
-Set-PSReadLineKeyHandler -Key ')',']','}' `
-                         -BriefDescription SmartCloseBraces `
-                         -LongDescription "Insert closing brace or skip" `
-                         -ScriptBlock {
-    param($key, $arg)
-
-    $line = $null
-    $cursor = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-
-    if ($line[$cursor] -eq $key.KeyChar)
-    {
-        [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-    }
-    else
-    {
-        [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)")
-    }
-}
-
-Set-PSReadLineKeyHandler -Key Backspace `
-                         -BriefDescription SmartBackspace `
-                         -LongDescription "Delete previous character or matching quotes/parens/braces" `
-                         -ScriptBlock {
-    param($key, $arg)
-
-    $line = $null
-    $cursor = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-
-    if ($cursor -gt 0)
-    {
-        $toMatch = $null
-        if ($cursor -lt $line.Length)
-        {
-            switch ($line[$cursor])
-            {
-                <#case#> '"' { $toMatch = '"'; break }
-                <#case#> "'" { $toMatch = "'"; break }
-                <#case#> ')' { $toMatch = '('; break }
-                <#case#> ']' { $toMatch = '['; break }
-                <#case#> '}' { $toMatch = '{'; break }
-            }
-        }
-
-        if ($toMatch -ne $null -and $line[$cursor-1] -eq $toMatch)
-        {
-            [Microsoft.PowerShell.PSConsoleReadLine]::Delete($cursor - 1, 2)
-        }
-        else
-        {
-            [Microsoft.PowerShell.PSConsoleReadLine]::BackwardDeleteChar($key, $arg)
-        }
-    }
-}
-
-#endregion Smart Insert/Delete
-
-# 保存命令而不执行
-$parameters = @{
-    Key = 'Alt+w'
-    BriefDescription = 'SaveInHistory'
-    LongDescription = 'Save current line in history but do not execute'
-    ScriptBlock = {
-      param($key, $arg)   # The arguments are ignored in this example
-
-# GetBufferState gives us the command line (with the cursor position)
-      $line = $null
-      $cursor = $null
-      [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line,
-        [ref]$cursor)
-
-# AddToHistory saves the line in history, but does not execute it.
-      [Microsoft.PowerShell.PSConsoleReadLine]::AddToHistory($line)
-
-# RevertLine is like pressing Escape.
-      [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-  }
-}
-Set-PSReadLineKeyHandler @parameters
-
-Set-PSReadLineKeyHandler -Key RightArrow `
-                         -BriefDescription ForwardCharAndAcceptNextSuggestionWord `
-                         -LongDescription "Move cursor one character to the right in the current editing line and accept the next word in suggestion when it's at the end of current editing line" `
-                         -ScriptBlock {
-    param($key, $arg)
-
-    $line = $null
-    $cursor = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-
-    if ($cursor -lt $line.Length) {
-        [Microsoft.PowerShell.PSConsoleReadLine]::ForwardChar($key, $arg)
-    } else {
-        [Microsoft.PowerShell.PSConsoleReadLine]::AcceptNextSuggestionWord($key, $arg)
-    }
-}
-
-
-Set-PSReadLineKeyHandler -Key Alt+q -LongDescription "exit" -ScriptBlock {
-    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("exit")
-    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-}
-
-## Add argument completer for the dotnet CLI tool
+#region dotnet 命令行补全
 $scriptblock = {
     param($wordToComplete, $commandAst, $cursorPosition)
     dotnet complete --position $cursorPosition $commandAst.ToString() |
@@ -367,26 +42,29 @@ $scriptblock = {
         }
 }
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock $scriptblock
+#endregion
 
-
+# 关闭 git merge 自动编辑功能，避免在合并时弹出编辑器
 $env:GIT_MERGE_AUTOEDIT = "no"
 
-
-function proxy {
+# 设置代理
+function set-proxy {
     $env:http_proxy = "http://127.0.0.1:7897"
     $env:https_proxy = "http://127.0.0.1:7897"
     [System.Net.WebRequest]::DefaultWebProxy = New-Object System.Net.WebProxy("http://127.0.0.1:7897")
     Write-Host "Proxy enabled: http://127.0.0.1:7897" -ForegroundColor Green
 }
 
-function unproxy {
+# 取消代理设置
+function clear-proxy {
     $env:http_proxy = $null
     $env:https_proxy = $null
     [System.Net.WebRequest]::DefaultWebProxy = $null
-    Write-Host "Proxy disabled" -ForegroundColor Yellow
+    Write-Host "Proxy cleared." -ForegroundColor Green
 }
 
-function check-proxy {
+# 获取代理设置
+function get-proxy {
     if ($env:http_proxy -or $env:https_proxy) {
         Write-Host "Current proxy settings:" -ForegroundColor Cyan
         Write-Host "HTTP Proxy: $env:http_proxy"
@@ -396,9 +74,46 @@ function check-proxy {
     }
 }
 
-
-# 搜索文本的简易 grep 命令
+# 设置 grep 命令的别名为 Select-String，方便在 PowerShell 中使用类似 Unix 的 grep 功能
 Set-Alias grep Select-String
 
+# 创建或更新文件的最后修改时间，类似于 Unix 的 touch 命令
+function touch {
+    param (
+        [string]$path
+    )
+    if (!(Test-Path $path)) {
+        New-Item -ItemType File -Path $path
+    } else {
+        (Get-Item $path).LastWriteTime = Get-Date
+    }
+}
 
+# 打开当前目录或指定目录的文件资源管理器
+function open {
+    param (
+        [string]$path = (Get-Location)
+    )
+    Start-Process "explorer.exe" $path
+}
 
+# 列出当前目录下的所有文件和文件夹，显示详细信息
+function ll {
+    Get-ChildItem
+}
+
+# 删除指定目录，如果不指定目录，则删除当前目录下的所有文件和文件夹，类似于 Unix 的 rm -rf 命令
+function rmrf {
+    param (
+        [string]$path = (Get-Location)
+    )
+    Remove-Item -Path $path -Recurse -Force
+}
+
+# 列出已安装模块
+function list-modules {
+    Get-InstalledModule
+}
+
+# 设置 oh-my-posh 主题
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\avit.omp.json" | Invoke-Expression
